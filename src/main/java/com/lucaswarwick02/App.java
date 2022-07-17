c2e4131fa4984bbce6a5fac058830f3c78189dc6
Lucas <lucaswarwick02@gmail.com>
2022-07-17 21:31:19 +0100

    Add to logging for WorldData

--- src/main/java/com/lucaswarwick02/App.java
+++ src/main/java/com/lucaswarwick02/App.java
@@ -6,6 +6,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.sql.SQLOutput;
+import java.util.HashMap;
+
 /**
  * Hello world!
  *
--- src/main/java/com/lucaswarwick02/content/WorldData.java
+++ src/main/java/com/lucaswarwick02/content/WorldData.java
@@ -1,6 +1,9 @@
 package com.lucaswarwick02.content;
 
+import com.lucaswarwick02.WorldState;
 import org.json.JSONArray;
+
+import java.util.HashMap;
 
 public class WorldData {
     public long time;
@@ -23,14 +26,40 @@ public class WorldData {
 
     @Override
     public String toString () {
+        StringBuilder sortiesString = new StringBuilder();
+        sortiesString.append("########## SORTIES ##########\n");
+        WorldState.INSTANCE.worldData.sorties.toList().forEach(entry -> {
+            ((HashMap) entry).forEach((k, v) -> {
+                sortiesString.append(k + ": " + v.getClass() + "\n");
+            });
+            sortiesString.append("\n\n");
+        });
+
+        StringBuilder invasionsString = new StringBuilder();
+        invasionsString.append("########## INVASIONS ##########\n");
+        WorldState.INSTANCE.worldData.invasions.toList().forEach(entry -> {
+            ((HashMap) entry).forEach((k, v) -> {
+                invasionsString.append(k + ": " + v.getClass() + "\n");
+            });
+            invasionsString.append("\n\n");
+        });
+
+        StringBuilder activeMissionsString = new StringBuilder();
+        activeMissionsString.append("########## ACTIVE MISSIONS ##########\n");
+        WorldState.INSTANCE.worldData.invasions.toList().forEach(entry -> {
+            ((HashMap) entry).forEach((k, v) -> {
+                activeMissionsString.append(k + ": " + v.getClass() + "\n");
+            });
+            activeMissionsString.append("\n\n");
+        });
 
         return this.time +
                 "\n\n" +
-                this.sorties.toString() +
+                sortiesString.toString() +
                 "\n\n" +
-                this.invasions.toString() +
+                invasionsString.toString() +
                 "\n\n" +
-                this.activeMissions.toString() +
+                activeMissionsString.toString() +
                 "\n\n" +
                 this.voidStorms.toString() +
                 "\n\n" +
